67b002be4018c7fc2b8afd3997a5018e7961ad4a
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-11 21:04:30 +0200

    Update starterkit for v1.76.2-rc.1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,15 +8,26 @@
 import { useState } from 'react';
 import type CreativeEngine from '@cesdk/engine';
 import type { Configuration } from '@cesdk/engine';
+import {
+  ImageColorsAssetSource,
+  ColorPaletteAssetSource,
+  DemoAssetSources,
+  StickerAssetSource,
+  TextAssetSource,
+  TextComponentAssetSource,
+  TypefaceAssetSource,
+  UploadAssetSources,
+  VectorShapeAssetSource
+} from '@cesdk/cesdk-js/plugins';
 import classes from './App.module.css';
 import { EditorProvider } from './contexts/EditorContext';
 import { PageSettingsProvider } from './contexts/PageSettingsContext';
-import PostcardUI from '@/app/layout/PostcardUI/PostcardUI';
-import { EngineProvider } from '@/app/contexts/EngineContext';
-import { SinglePageModeProvider } from '@/app/contexts/SinglePageModeContext';
-import { SelectionProvider } from '@/app/contexts/SelectionContext';
-import LoadingSpinner from '@/app/components/LoadingSpinner/LoadingSpinner';
-import { initPostcardEditor } from '@/imgly';
+import PostcardUI from './PostcardUI/PostcardUI';
+import { EngineProvider } from '../imgly/contexts/EngineContext';
+import { SinglePageModeProvider } from '../imgly/contexts/SinglePageModeContext';
+import { SelectionProvider } from '../imgly/contexts/SelectionContext';
+import LoadingSpinner from './ui/LoadingSpinner/LoadingSpinner';
+import { createUnsplashSource } from '../imgly';
 
 interface AppProps {
   engineConfig: Partial<Configuration>;
@@ -36,7 +47,55 @@ const App: React.FC<AppProps> = ({ engineConfig }) => {
 
   const configureEngine = async (engine: CreativeEngine) => {
     setEngine(engine);
-    await initPostcardEditor(engine);
+    engine.editor.setSetting('page/title/show', false);
+
+    // Add default asset sources via plugins
+    await engine.addPlugin(new ImageColorsAssetSource());
+    await engine.addPlugin(new ColorPaletteAssetSource());
+    await engine.addPlugin(new TypefaceAssetSource());
+    await engine.addPlugin(new TextAssetSource());
+    await engine.addPlugin(new TextComponentAssetSource());
+    await engine.addPlugin(
+      new VectorShapeAssetSource({
+        include: ['ly.img.vector.shape.filled.*']
+      })
+    );
+    await engine.addPlugin(new StickerAssetSource());
+
+    // Add demo asset sources via plugins
+    await engine.addPlugin(
+      new UploadAssetSources({
+        include: [
+          'ly.img.image.upload',
+          'ly.img.video.upload',
+          'ly.img.audio.upload'
+        ]
+      })
+    );
+    await engine.addPlugin(
+      new DemoAssetSources({
+        include: [
+          'ly.img.templates.design.*',
+          'ly.img.audio.*',
+          'ly.img.video.*'
+        ]
+      })
+    );
+
+    engine.editor.setGlobalScope('lifecycle/destroy', 'Defer');
+    engine.editor.setRole('Adopter');
+
+    engine.asset.addSource(createUnsplashSource(engine));
+
+    const stickers = await engine.asset.findAssets('ly.img.sticker', {
+      page: 0,
+      perPage: 9999
+    });
+    stickers.assets.forEach((sticker) => {
+      if (sticker.groups[0] !== 'emoticons') {
+        engine.asset.removeAssetFromSource('ly.img.sticker', sticker.id);
+      }
+    });
   };
 
   return (
